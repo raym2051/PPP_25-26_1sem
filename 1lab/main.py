16a3cb47a02b91fbe9b7ca8d13b3cb8f7df4e573
raym2051 <inli.raym.16@gmail.com>
2025-09-29 22:19:43 +0300

    Update main.py

--- 1lab/main.py
+++ 1lab/main.py
@@ -1,4 +1,24 @@
+if __name__ == "__mane__":
+    from random import *
 
+    dano = []
+    for i in range(1, *sample(range(2,1000),1)):
+        dano.append(i)
 
-if __name__ == "__main__":
-    pass # Ваш код здесь
+    n = sample(range(2,1000),1)
+
+    count = 0
+    ans = set()
+
+    for _ in range(n[0]):
+        shuffle(dano)
+        for number in dano:
+            if dano.index(number) + 1 == number:
+                count += 1
+                ans.add(number)
+
+    ans = sorted(ans)
+
+    print(f'Частота попадания числа на свое место, при {n[0]} тусовках = {round(count / len(dano),2) * 100:.0f} раз')
+    print(f'Вероятность, что число попадет на свое место = {round(len(ans)/len(dano),2) * 100:.0f}%')
+    print(f'Числа которые повторялись от 1 до {max(dano)}: {ans}.')
