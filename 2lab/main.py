3995063083357abf7e2e8487bfb39a9bfc81a624
raym2051 <inli.raym.16@gmail.com>
2025-10-14 16:33:54 +0300

    Update main.py

--- 2lab/main.py
+++ 2lab/main.py
@@ -14,30 +14,65 @@ if __name__ == "__main__":
         'M': 1000
     }
     
-    def number(rom):
+    def rom_number(rom):
         if '-' in rom or '+' in rom or '*' in rom or '/' in rom:
             return rom
         else:
-            arab_number = 0
+            arab = 0
             total = 0
             for index in rom[::-1]:
                 rec = roman_data[index]
                 if rec < total:
-                    arab_number -= rec
+                    arab -= rec
                 else:
-                    arab_number += rec
+                    arab += rec
                 total = rec
             return arab
+    
+    def arab_number(arab):
+        line = ''
+        while arab > 0:
+    
+            if arab - roman_data['M'] > 0:
+                line += 'M'
+                arab -= roman_data['M']
+    
+            elif arab - roman_data['D'] > 0:
+                line += 'D'
+                arab -= roman_data['D']
+    
+            elif arab - roman_data['C'] > 0:
+                line += 'C'
+                arab -= roman_data['C']
+    
+            elif arab - roman_data['L'] > 0:
+                line += 'L'
+                arab -= roman_data['L']
+    
+            elif arab - roman_data['X'] > 0:
+                line += 'X'
+                arab -= roman_data['X']
+    
+            elif arab - roman_data['V'] > 0:
+                line += 'V'
+                arab -= roman_data['V']
+    
+            else:
+                line += 'I'
+                arab -= roman_data['I']
+    
+        return line
     
     def calculation(sting):
         list = sting.split(' ')
         line = ''
         for subject in list:
-            line += str(number(subject))
-        number_of_end = eval(line)
-        if number_of_end <= 0:
+            line += str(rom_number(subject))
+    
+        number = eval(line)
+        if number <= 0:
             return 'Операция невозможна'
         else:
-            return number_of_end
+            return arab_number(number)
     
     print(f'Результат операции: {calculation(roman_number)}')
