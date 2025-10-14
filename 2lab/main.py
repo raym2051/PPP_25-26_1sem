5e54fea2402347c1687cbd5067ae04b194e22d50
raym2051 <inli.raym.16@gmail.com>
2025-10-14 16:17:30 +0300

    Update main.py

--- 2lab/main.py
+++ 2lab/main.py
@@ -1,4 +1,43 @@
 
 
 if __name__ == "__main__":
-    pass # Ваш код здесь
+    print('Возможные операции: +; -; *; /')
+    roman_number = input('Введите римсике цифры и операции: ')
+    
+    roman_data = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    def number(rom):
+        if '-' in rom or '+' in rom or '*' in rom or '/' in rom:
+            return rom
+        else:
+            arab_number = 0
+            total = 0
+            for index in rom[::-1]:
+                rec = roman_data[index]
+                if rec < total:
+                    arab_number -= rec
+                else:
+                    arab_number += rec
+                total = rec
+            return arab
+    
+    def calculation(sting):
+        list = sting.split(' ')
+        line = ''
+        for subject in list:
+            line += str(number(subject))
+        number_of_end = eval(line)
+        if number_of_end <= 0:
+            return 'Операция невозможна'
+        else:
+            return number_of_end
+    
+    print(f'Результат операции: {calculation(roman_number)}')
